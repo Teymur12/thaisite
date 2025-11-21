8f69e7d85fb1a20299530c9bf9d82efb3f799fa0
Teymur12 <126714102+Teymur12@users.noreply.github.com>
2025-11-21 10:21:46 +0400

    Create head.js

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>Thai Health Therapy - Masaj və Relaksasiya Mərkəzi</title>
+      <meta name="description" content="Peşəkar Thai masajı və relaksasiya xidmətləri" />
+      <link rel="icon" href="/favicon.ico" sizes="32x32" />
+      <link rel="icon" type="image/png" href="/icon-192.png" sizes="192x192" />
+      <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+    </>
+  );
