4ec3710ebdf4e9ebaadadba182882c766364c9c4
Teymur12 <126714102+Teymur12@users.noreply.github.com>
2025-11-24 16:17:18 +0400

    Create LayoutClient.js

---
+++ src/app/LayoutClient.js
@@ -0,0 +1,21 @@
+// app/LayoutClient.js
+"use client"
+
+import { usePathname } from "next/navigation"
+import { useEffect, useState } from "react"
+import Loading from "./loading"
+
+export default function LayoutClient({ children }) {
+  const pathname = usePathname()
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    setLoading(true)
+    const timer = setTimeout(() => setLoading(false), 300)
+    return () => clearTimeout(timer)
+  }, [pathname])
+
+  if (loading) return <Loading />
+
+  return children
+}
